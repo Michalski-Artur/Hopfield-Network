12d5bea3b57575a8a3542840feec68055c482a71
Artur Michalski <armic@eg.dk>
2022-11-27 23:04:49 +0100

    Experiments with oja rule

--- learning_rules.py
+++ learning_rules.py
@@ -4,42 +4,37 @@ import numpy as np
 
 LearningRuleType = Callable[[np.ndarray], np.ndarray]
 
-MAX_ITERATIONS = 10
+MAX_ITERATIONS = 50
+
 
 class LearningRules:
 
     @staticmethod
     def hebb(patterns: np.ndarray):
-        weights = (1 / patterns.shape[0]) * patterns.T @ patterns
+        weights = patterns.T @ patterns / patterns.shape[0]
         np.fill_diagonal(weights, 0)
         return weights
 
     @staticmethod
     def oja(patterns: np.ndarray):
         weights = LearningRules.hebb(patterns)
-        factor = patterns.shape[1] * patterns.shape[1]
+        #weights = np.zeros((patterns.shape[1], patterns.shape[1])) + 1 #Maybe better ? Or initialize with random?
         prev_dif = np.inf
         prev_weights = weights
         for iter in range(MAX_ITERATIONS):
             print(f'Learning patterns with Oja\'s rule: iteration {iter + 1}/{MAX_ITERATIONS}...')
-            delta_weights = np.zeros(weights.shape)
-            y_array = weights @ patterns.T
-            for i in range(patterns.shape[1]):
-                for j in range(patterns.shape[1]):
-                    if i == j:
-                        continue
-                    delta_weights[i, j] = y_array[i, :] @ (patterns[:, j] - weights[i, j] * y_array[i, :]).T
-            delta_weights = delta_weights / factor
+            delta_weights = ((patterns.T @ patterns) / patterns.shape[0] * weights -
+                             weights.T * ((patterns.T @ patterns) / patterns.shape[0]) * weights * weights / 2) / 4
             weights = weights + delta_weights
             diff = np.linalg.norm(delta_weights)
             print(f'Delta = {diff}')
             if diff < 1e-5:
-                print (f'Learning converged after {iter} iterations')
+                print(f'Learning converged after {iter} iterations')
                 break
-            if diff - prev_dif > 1e-5:
-                print (f'Learning diverged after {iter} iterations')
+            if abs(diff - prev_dif) < 1e-5:
+                print(f'Learning diverged after {iter} iterations')
                 weights = prev_weights
                 break
             prev_dif = diff
             prev_weights = weights
-        return weights
+        return weights - 1
